6a072cbf468fd1994e321c63a5f3805ba90258d6
kchihogi <kchihogi@gmail.com>
2024-01-23 08:19:33 +0900

    paginationをCustom

--- server/proj/proj/settings.py
+++ server/proj/proj/settings.py
@@ -152,8 +152,8 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'app.pagination.CustomPagination',
+    'PAGE_SIZE': 10,
     'URL_FIELD_NAME': 'href',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
